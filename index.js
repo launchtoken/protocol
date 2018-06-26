98d120031a9e1f0c3ea6862c8601d8b8cdf4eba5
melnx <lyosha@buystand.com>
2018-06-26 13:41:36 -0700

    add index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+
+var config = {port:1337};
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/launchtoken.html');
+});
+
+app.get('/node_modules/web3/dist/web3.min.js', function (req, res) {
+    res.sendFile(__dirname + '/node_modules/web3/dist/web3.min.js');
+});
+
+
+//app.use("/", express.static(__dirname + '/public'));
+
+var http_port = process.env.PORT || config.port;
+
+var server = http.createServer(app);
+
+server.listen(http_port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Evtend app listening at http://%s:%s', host, port);
+});
